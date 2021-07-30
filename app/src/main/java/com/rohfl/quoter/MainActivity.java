76d94f6d8c98cb994d76c76a28b4d4ab4c88f231
rohfl <iamrohitjangid21@gmail.com>
2021-07-31 01:19:58 +0530

    Complete project.
    Add Internet Permission, use Volley for HTTP requests and change some theme colors.

--- app/src/main/java/com/rohfl/quoter/MainActivity.java
+++ app/src/main/java/com/rohfl/quoter/MainActivity.java
@@ -1,14 +1,78 @@
 package com.rohfl.quoter;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.rohfl.quoter.singleton.MySingleton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
+
+/**
+ * MainActivity
+ * @author Rohit Jangid
+ * @author https://www.github.com/rohfl
+ * @version 1.0
+ * @since 1.0
+ */
 public class MainActivity extends AppCompatActivity {
+
+    // views
+    MaterialButton getQuoteButton;
+    MaterialTextView quoteTV,authorTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // views
+        quoteTV = (MaterialTextView) findViewById(R.id.quote_tv);
+        authorTV = (MaterialTextView) findViewById(R.id.author_tv);
+        getQuoteButton = (MaterialButton) findViewById(R.id.get_quote_button);
+
+        // load a quote when first time user opens the app
+        getData();
+
+        // setting the onClickListener on the button, using lambda for less clutter
+        // and better readability
+        getQuoteButton.setOnClickListener(view -> {
+            getData();
+        });
+
     }
+
+    /**
+     * fetches the data from the API
+     */
+    private void getData() {
+        // the API Url
+        String QUOTE_URL = "https://api.quotable.io/random";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, QUOTE_URL,null,
+                response -> {
+                    try {
+
+                        // getting the strings from the keys content and author
+                        String content = response.getString("content");
+                        String author = response.getString("author");
+
+                        // setting the content in the views
+                        quoteTV.setText(content);
+                        authorTV.setText("- " + author);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+                    error.printStackTrace();
+                });
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
 }
--- app/src/main/java/com/rohfl/quoter/singleton/MySingleton.java
+++ app/src/main/java/com/rohfl/quoter/singleton/MySingleton.java
@@ -6,16 +6,33 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-
+/**
+ * MySingleton class will be used to create only one object throughout the app lifecycle
+ * @author Rohit Jangid
+ * @author https://www.github.com/rohfl
+ * @version 1.0
+ * @since 1.0
+ */
 public class MySingleton {
+
     private static MySingleton mInstance;
     private RequestQueue mRequestQueue;
     private static Context mContext;
+
+    /**
+     * Private Constructor
+     * @param context context of the activity which first called the getInstance method of this class
+     */
     private MySingleton(Context context){
         mContext = context;
         mRequestQueue = getRequestQueue();
     }
 
+    /**
+     *
+     * @param context context of the calling activity
+     * @return the object
+     */
     public static synchronized MySingleton getInstance(Context context){
         if(mInstance == null){
             mInstance = new MySingleton(context);
@@ -23,6 +40,10 @@ public class MySingleton {
         return mInstance;
     }
 
+    /**
+     * Creates the RequestQueue object
+     * @return RequestQueue object
+     */
     public RequestQueue getRequestQueue(){
         if(mRequestQueue == null){
             mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
@@ -30,6 +51,11 @@ public class MySingleton {
         return mRequestQueue;
     }
 
+    /**
+     * Adds request to the queue
+     * @param request the request which we want to add in the queue
+     * @param <T> generic
+     */
     public<T> void addToRequestQueue(Request<T> request){
         getRequestQueue().add(request);
     }
